9ae5a5b394f5808b05488e6829deb03b4c4caaf7
Sai Hemanth <saihemanth9019@gmail.com>
2022-09-18 17:01:48 +0530

    Add sample example

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,26 @@
 function component() {
-  const element = document.createElement('div');
 
-  const foo = 100;
-  const bar = 1000;
-  console.log(foo === bar);
+  function calculator(op, a, b) {
+    if (op == '+') {
+      return a + b;
+    } else if (op == '-') {
+      return a - b;
+    } else if (op == '*') {
+      return a * b;
+    } else {
+      return -1;
+    }
+  }
+
+  const element = document.createElement('div');
+  const adder = calculator('+');
+  const subtractor = calculator('-');
+  const multiplier = calculator('*');
+
+  console.log(adder(10, 20));
+  console.log(multiplier(10, 20));
+  console.log(subtractor(adder(1, 2), multiplier(10, 2)));
+
   return element;
 }
 
