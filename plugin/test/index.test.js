d9aeb5d889e3521843c08e6abe8af557b4271565
Sai Hemanth <saihemanth9019@gmail.com>
2022-09-18 13:06:12 +0530

    Refactor jest unit tests

--- plugin/test/index.test.js
+++ plugin/test/index.test.js
@@ -21,16 +21,16 @@ import hjsPlugin from '../src';
  * 19. [X] How would classes work with this enabled?
  * 20. [X] What happens to class constructors?
  * 21. [X] How does "this" keyword work in such curried functions?
- * 22. Using a function constructor
+ * 22. [X] Using a function constructor
  * 23. Passing callback functions
- * 24. Javascript decorator functions
- * 25. Arrow functions with spread operator
- * 26. Default parameters in arrow functions. Support rest parameter syntax
+ * 24. Arrow functions with spread operator
+ * 25. Default parameters in arrow functions. Support rest parameter syntax
  */
 
-pluginTester({
+ pluginTester({
     plugin: hjsPlugin,
-    title: 'Base tests',
+    title: 'Basic Function tests',
+    // filename: 'basic-function-tests',
     filename: __filename,
     fixtures: path.join(__dirname, '__fixtures__'),
     tests: [
@@ -50,6 +50,21 @@ pluginTester({
             snapshot: true,
         },
         {
+            title: 'nested functions',
+            fixture: '__fixtures__/nested-func-test.js',
+            snapshot: true,
+        },
+    ]
+});
+
+pluginTester({
+    plugin: hjsPlugin,
+    title: 'Basic Function Expression tests',
+    //filename: 'function-expr-tests',
+    filename: __filename,
+    fixtures: path.join(__dirname, '__fixtures__'),
+    tests: [
+        {
             title: 'anonymous function expression with 3 parameters',
             fixture: '__fixtures__/3-param-anon-FE-test.js',
             snapshot: true,
@@ -65,35 +80,20 @@ pluginTester({
             snapshot: true,
         },
         {
-            title: 'nested functions',
-            fixture: '__fixtures__/nested-func-test.js',
-            snapshot: true,
-        },
-        {
-            title: 'with default parameters at the end',
-            fixture: '__fixtures__/default-param-test.js',
-            snapshot: true,
-        },
-        {
-            title: 'with default parameters in between ',
-            fixture: '__fixtures__/partial-default-param-test.js',
-            snapshot: true,
-        },
-        {
             title: 'iife',
             fixture: '__fixtures__/iife-test.js',
             snapshot: true,
         },
-        {
-            title: 'with one rest parameters field',
-            fixture: '__fixtures__/rest-param-test.js',
-            snapshot: true,
-        },
-        {
-            title: 'async function declaration',
-            fixture: '__fixtures__/async-func-test.js',
-            snapshot: true,
-        },
+    ],
+});
+
+pluginTester({
+    plugin: hjsPlugin,
+    title: 'Async Function tests',
+    //filename: 'async-func-tests',
+    filename: __filename,
+    fixtures: path.join(__dirname, '__fixtures__'),
+    tests: [
         {
             title: 'async function expression',
             fixture: '__fixtures__/async-func-expr-test.js',
@@ -105,10 +105,20 @@ pluginTester({
             snapshot: true,
         },
         {
-            title: 'function using parent object\'s variables',
-            fixture: '__fixtures__/preserve-this-test.js',
+            title: 'async function declaration',
+            fixture: '__fixtures__/async-func-test.js',
             snapshot: true,
         },
+    ],
+});
+
+pluginTester({
+    plugin: hjsPlugin,
+    title: 'Special Function tests',
+    //filename: 'special-func-tests',
+    filename: __filename,
+    fixtures: path.join(__dirname, '__fixtures__'),
+    tests: [
         {
             title: 'generator function',
             fixture: '__fixtures__/generator-test.js',
@@ -129,6 +139,66 @@ pluginTester({
             fixture: '__fixtures__/function-constr-test.js',
             snapshot: true,
         }, 
+    ],
+});
+
+pluginTester({
+    plugin: hjsPlugin,
+    title: 'Function context tests',
+    //filename: 'func-context-tests',
+    filename: __filename,
+    fixtures: path.join(__dirname, '__fixtures__'),
+    tests: [
+        {
+            title: 'function using parent object\'s variables',
+            fixture: '__fixtures__/preserve-this-test.js',
+            snapshot: true,
+        },
+    ],
+});
+
+pluginTester({
+    plugin: hjsPlugin,
+    title: 'Default Parameters tests',
+    //filename: 'default-params-tests',
+    filename: __filename,
+    fixtures: path.join(__dirname, '__fixtures__'),
+    tests: [
+        {
+            title: 'with default parameters at the end',
+            fixture: '__fixtures__/default-param-test.js',
+            snapshot: true,
+        },
+        {
+            title: 'with default parameters in between ',
+            fixture: '__fixtures__/partial-default-param-test.js',
+            snapshot: true,
+        },
+    ],
+});
+
+pluginTester({
+    plugin: hjsPlugin,
+    title: 'Rest Parameter tests',
+    //filename: 'rest-params-tests',
+    filename: __filename,
+    fixtures: path.join(__dirname, '__fixtures__'),
+    tests: [
+        {
+            title: 'with one rest parameters field',
+            fixture: '__fixtures__/rest-param-test.js',
+            snapshot: true,
+        },
+    ],
+});
+
+pluginTester({
+    plugin: hjsPlugin,
+    title: 'Class tests',
+    //filename: 'class-tests',
+    filename: __filename,
+    fixtures: path.join(__dirname, '__fixtures__'),
+    tests: [
         {
             title: 'basic class with constructor',
             fixture: '__fixtures__/class-test.js',
@@ -143,6 +213,7 @@ pluginTester({
             title: 'basic function prototype',
             fixture: '__fixtures__/function-prototype-test.js',
             snapshot: true,
-        }, 
-    ]
+        },
+    ],
 });
+
